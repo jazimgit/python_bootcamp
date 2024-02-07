42e48ca4fccfbde36a14f1fc70215e0b4d3e6829
jazimgit <jazim2211@gmail.com>
2024-02-07 19:07:44 +0530

    added code

---
+++ bootcamp/opr1.py
@@ -0,0 +1,20 @@
+#with greter than oprator
+print( "welcome to frist oprater program")
+hight=int(input("please type here no :"))
+
+print (f"you typed hight {hight}")
+
+if hight > 120:
+    print ("hight is to longggg")
+else:
+    print (f"hight is short {hight}")
+
+
+#for equal "=" we need to use "==" Python programming
+
+print ("please input your name")
+name=str(input ("what is your name ?"))  
+if name == "sahil":
+    print ("name is correct " + name )
+else:
+    print ("name is not correct")
---
+++ bootcamp/printf.py
@@ -0,0 +1,5 @@
+hi = 25
+hb = 35
+no = False
+
+print (f"hi my name is sahil and my adge is {hi} and my bothers age is {hb} and my result is {no}")
